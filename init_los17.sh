0441b75c821fb5d6027454318f65a6a9399130c2
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2021-04-07 11:24:38 +0200

    Cleanup

--- init_los17.sh
+++ init_los17.sh
@@ -16,7 +16,7 @@ if [ -d vendor/fdroid ] ; then
   cd ../..
 fi
 
-cd bionic/
+cd bionic
   git fetch "https://github.com/TALUAtGitHub/android_bionic" lineage-17.1 && git cherry-pick e1e3f9df123f8777bc2831859c4b62544c777117
 cd ..
 
@@ -32,7 +32,6 @@ cd packages/apps/Eleven
   git fetch "https://github.com/LineageOS/android_packages_apps_Eleven" refs/changes/25/306325/4 && git cherry-pick FETCH_HEAD
 cd ../../..
 
-cd system/vold/
+cd system/vold
   git fetch "https://github.com/TALUAtGitHub/android_system_vold" lineage-17.1 && git cherry-pick 0a5a4c5805832c68e1e1f224db7f4bee0ac1f83d
 cd ../..
-
