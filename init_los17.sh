480dbfd27b28d42f937cee6711213c99c386c86f
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2021-04-14 11:53:43 +0200

    Update to match the latest changes

--- init_los17.sh
+++ init_los17.sh
@@ -21,11 +21,11 @@ cd bionic
 cd ..
 
 cd frameworks/av
-  git fetch "https://github.com/TALUAtGitHub/android_frameworks_av" lineage-17.1 && git cherry-pick 59516cd4d9158ac5991d3beec79b4e6617b1c5b1^..f79ac375966db9bf6dc2f16be96dd02fb70d81ee
+  git fetch "https://github.com/TALUAtGitHub/android_frameworks_av" lineage-17.1 && git cherry-pick fe0f4a374df8d61ede5d880e0df08071988a4293^..f04447abfb61aee3fd817a53759277603335cc85
 cd ../..
 
 cd frameworks/base
-  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-17.1 && git cherry-pick 5d727004f90d011c033023e78da10321a94165d1^..2d3b941d9a1e77cc226c75215a5598739f8843d9
+  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-17.1 && git cherry-pick 9b9e13d38b450404015067f5416f756229bf27d7^..2d10bd7a63fd7d27a93d98c030ffcc0ca07c9e04
 cd ../..
 
 cd packages/apps/Eleven
