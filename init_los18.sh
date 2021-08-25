61587bfbb0d20cad4ace2b59a53b7d1eda7f21cf
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2021-08-25 10:36:36 +0200

    Update to match the latest changes

--- init_los18.sh
+++ init_los18.sh
@@ -14,7 +14,7 @@ if [ -d vendor/fdroid ] ; then
 fi
 
 cd frameworks/base
-  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-18.1 && git cherry-pick ab25e65bc64bce37f0057cfe1ab9c8803f5fa931
+  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-18.1 && git cherry-pick e9e24b05322e03c0b2049600c85ff538bbed3c98
 cd ../..
 
 cd packages/modules/NetworkStack
