af05261269562d13efe09aa3159b26dc457e71db
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2021-10-20 16:32:45 +0200

    Update to match the latest changes

--- init_los17.sh
+++ init_los17.sh
@@ -17,5 +17,5 @@ if [ -d vendor/fdroid ] ; then
 fi
 
 cd frameworks/base
-  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-17.1 && git cherry-pick 3e618628677ae036004d6aa1b386c03f399401b3
+  git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-17.1 && git cherry-pick 4acceb4bed138015f8cc482a57c39133c67a2579
 cd ../..
