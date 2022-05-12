be9b08292bb9fc053bd71e28786557cd17eb6bb8
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2022-05-12 09:31:15 +0200

    Update to match the latest changes

--- init_los19.sh
+++ init_los19.sh
@@ -21,20 +21,13 @@ cd bionic
 	git fetch "https://github.com/Claymore1297/android_bionic" lineage-19.0 && git cherry-pick da090ee04042f64b96e7d15e32a80f4daf8743e6
 cd ..
 
+cd frameworks/av
+	git fetch "https://github.com/TARKZiM/android_frameworks_av" lineage-19.1 && git cherry-pick 57a73f758a513c8fdbe379a438c635a42b779f33^..cf5d64d587531a13a09cb1ed3814c7d6e4e2a12f
+cd ../..
+
 cd frameworks/base
-	git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-19.1 && git cherry-pick 675673a98d0f54356518db2544c0efffc7837943
-	git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/34/324734/3 && git cherry-pick FETCH_HEAD
 	git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/92/326692/1 && git cherry-pick FETCH_HEAD
-	git fetch "https://github.com/TARKZiM/android_frameworks_base" lineage-19.0 && git cherry-pick a01b7f3b4c98d9226ac1e054316945565df8b05e
-# Caution!
-# ========
-# Be sure, that the files
-# frameworks/base/core/res/res/values/lineage_config.xml
-# and
-# frameworks/base/core/res/res/values/lineage_symbols.xml
-# are present before executing the next commands!
-	git add .
-	git commit
+	git fetch "https://github.com/TARKZiM/android_frameworks_base" lineage-19.1 && git cherry-pick 43557a700ee6782f8975a9515661be706782c9ae^..5c9c6cdba014804cafe43097f94c723388fc29a8
 cd ../..
 
 cd hardware/qcom-caf/msm8994/audio/
@@ -42,7 +35,7 @@ cd hardware/qcom-caf/msm8994/audio/
 cd ../../../..
 
 cd packages/modules/adb/
-	git fetch "https://github.com/LineageOS/android_packages_modules_adb" refs/changes/85/326385/1 && git cherry-pick FETCH_HEAD
+	git fetch "https://github.com/LineageOS/android_packages_modules_adb" refs/changes/85/326385/3 && git cherry-pick FETCH_HEAD
 cd ../../..
 
 cd packages/modules/NetworkStack
@@ -50,10 +43,18 @@ cd packages/modules/NetworkStack
 cd ../../..
 
 cd system/bpf
-	git fetch "https://github.com/LineageOS/android_system_bpf" refs/changes/91/320591/1 && git cherry-pick FETCH_HEAD
+	git fetch "https://github.com/LineageOS/android_system_bpf" refs/changes/91/320591/2 && git cherry-pick FETCH_HEAD
+cd ../..
+
+cd system/core
+	git fetch "https://github.com/TARKZiM/android_system_core" lineage-19.1 && git cherry-pick d4eb13ad0c6dda12307b242236d49b8db2ddb199
 cd ../..
 
 cd system/netd
-	git fetch "https://github.com/LineageOS/android_system_netd" refs/changes/92/320592/1 && git cherry-pick FETCH_HEAD
+	git fetch "https://github.com/LineageOS/android_system_netd" refs/changes/92/320592/2 && git cherry-pick FETCH_HEAD
 cd ../..
 
+cd vendor/lineage
+	git fetch "https://github.com/TARKZiM/android_vendor_lineage" lineage-19.1 && git cherry-pick 80ddec1a9f6bf2368a5a391f5bd44140158f3c53
+	git revert 8f67d055b36d992f2f09aa6f733aa06ee3d5b917
+cd ../..
