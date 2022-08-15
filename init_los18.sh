e108b5b76cee5e9d97b48e5b7f4c4c8023a28005
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2022-08-15 17:11:04 +0200

    Update to match the latest changes

--- init_los18.sh
+++ init_los18.sh
@@ -17,10 +17,6 @@ cd frameworks/base
   git fetch "https://github.com/TALUAtGitHub/android_frameworks_base" lineage-18.1 && git cherry-pick 746f575c7db85062e1cae013b8bc0fa6b80e89f4
 cd ../..
 
-cd hardware/lineage/interfaces
-	git fetch "https://github.com/LineageOS/android_hardware_lineage_interfaces" lineage-18.1 && git cherry-pick cfcb4c8a978a8627a5d6913089d9bb92d7356877
-cd ../../..
-
 cd packages/modules/NetworkStack
   git fetch "https://github.com/DerpFest-11/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
 cd ../../..
