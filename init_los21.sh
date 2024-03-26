c09c4980a1bd485245ccc1e05bd2e023ec732981
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2024-03-26 16:54:40 +0100

    It's better to cherry-pick my commits from my sepolicy repos.

--- init_los21.sh
+++ init_los21.sh
@@ -35,3 +35,11 @@ if [ -d vendor/microG ] ; then
     ./get_packages.sh
   cd ../..
 fi
+
+cd device/lineage/sepolicy
+  git fetch "https://github.com/Professor-Berni/android_device_lineage_sepolicy" lineage-21.0 && git cherry-pick fd626833804450dadb057d838a78f5b0f7756edb^..02cc887fdc94cec0201e6dfbfe12d37b41d2ef86
+cd ../../..
+
+cd system/sepolicy
+  git fetch "https://github.com/Professor-Berni/android_system_sepolicy" lineage-21.0 && git cherry-pick ca6ecbf2127062924476ac06d8df96cecdde68a6
+cd ../..
