16fe6121df6e1f7ed07e74bf5a25b7a520d21ba7
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2024-07-03 12:59:15 +0200

    Updated to match the latest changes.

--- init_los19.sh
+++ init_los19.sh
@@ -19,38 +19,38 @@ if [ -d vendor/microG ] ; then
 fi
 
 cd bootable/recovery
-	git revert 7c1eb300fbcf789024da690c96adb167d31aa5a5
+  git revert 7c1eb300fbcf789024da690c96adb167d31aa5a5
 cd ../..
 
 cd frameworks/base
-	git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/92/326692/1 && git cherry-pick FETCH_HEAD
+  git fetch "https://github.com/LineageOS/android_frameworks_base" refs/changes/92/326692/1 && git cherry-pick FETCH_HEAD
 cd ../..
 
 cd hardware/qcom-caf/msm8994/audio/
-	git fetch "https://github.com/LineageOS/android_hardware_qcom_audio" lineage-18.1-caf-msm8916 && git cherry-pick ce771ebbc1988db8ccefb20615a8ca6295c3e93a
+  git fetch "https://github.com/LineageOS/android_hardware_qcom_audio" lineage-18.1-caf-msm8916 && git cherry-pick ce771ebbc1988db8ccefb20615a8ca6295c3e93a
 cd ../../../..
 
 cd packages/modules/Permission/
-	git fetch "https://github.com/K9100ii/android_packages_modules_Permission" lineage-19.1 && git cherry-pick f07fcd68f11d2fbc26763985e3775f89fc7533ea
+  git fetch "https://github.com/K9100ii/android_packages_modules_Permission" lineage-19.1 && git cherry-pick f07fcd68f11d2fbc26763985e3775f89fc7533ea
 cd ../../..
 
 cd packages/modules/NetworkStack
-	git fetch "https://github.com/DerpFest-11/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
+  git fetch "https://github.com/DerpFest-11/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
 cd ../../..
 
 cd system/bpf
-	git fetch "https://github.com/K9100ii/android_system_bpf" android-12L && git cherry-pick a15ffea764407ad7a36edf10c939dc4a44b41d9b
+  git fetch "https://github.com/K9100ii/android_system_bpf" android-12L && git cherry-pick a15ffea764407ad7a36edf10c939dc4a44b41d9b
 cd ../..
 
 cd system/core
-	git fetch "https://github.com/LineageOS/android_system_core" refs/changes/12/317912/2 && git cherry-pick FETCH_HEAD
+  git fetch "https://github.com/LineageOS/android_system_core" refs/changes/12/317912/2 && git cherry-pick FETCH_HEAD
 cd ../..
 
 cd system/netd
-	git fetch "https://github.com/K9100ii/android_system_netd" lineage-19.1 && git cherry-pick 1f89ad475f265d2f461498cd73b36eacf2915e24
+  git fetch "https://github.com/K9100ii/android_system_netd" lineage-19.1 && git cherry-pick 1f89ad475f265d2f461498cd73b36eacf2915e24
 cd ../..
 
 cd vendor/lineage
-	git revert 8f67d055b36d992f2f09aa6f733aa06ee3d5b917
-	git revert f224255cd9869d289e9d3a186a40c51d1ea8876c
+  git revert 8f67d055b36d992f2f09aa6f733aa06ee3d5b917
+  git revert f224255cd9869d289e9d3a186a40c51d1ea8876c
 cd ../..
