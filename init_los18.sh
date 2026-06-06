06faa456ec66f4ba8fc3ad87aef1a260ab5e8d8f
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2026-06-06 14:07:30 +0200

    Update to match the latest changes.

--- init_los18.sh
+++ init_los18.sh
@@ -8,21 +8,17 @@ repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
 repo sync --force-sync
 
 if [ -d vendor/fdroid ] ; then
-  cd vendor/fdroid
-    ./get_packages.sh
-  cd ../..
+	cd vendor/fdroid
+		./get_packages.sh
+	cd ../..
 fi
 
 if [ -d vendor/microG ] ; then
-  cd vendor/microG
-    ./get_packages.sh
-  cd ../..
+	cd vendor/microG
+		./get_packages.sh
+	cd ../..
 fi
 
-cd frameworks/base
-	git fetch "https://github.com/K9100ii/android_frameworks_base" lineage-18.1 && git cherry-pick a27f890c8b5875c4255d6ba48c567379620bcaf0
-cd ../..
-
 cd packages/modules/NetworkStack
-	git fetch "https://github.com/DerpFest-11/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
+	git fetch "https://github.com/Professor-Berni/packages_modules_NetworkStack" 11 && git cherry-pick 22fd53a977eeaf4e36be7bf6358ecf2c2737fa5e
 cd ../../..
