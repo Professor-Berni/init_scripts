e9e5216586fbb5d84a60b67c01bf43a5ab1a3fcf
Bernhard Thoben <Professor-Berni@users.noreply.github.com>
2026-08-26 14:05:39 +0200

    Update to match the latest changes.

--- init_los23_2.sh
+++ init_los23_2.sh
@@ -41,3 +41,7 @@ cd $current_dir
 cd hardware/ril
   git fetch "https://github.com/Professor-Berni/android_device_sony_kitakami-common" lineage-21.0 && git cherry-pick 770260ee50f46af320ea04621c469f9d974a2920
 cd $current_dir
+
+cd system/sepolicy
+  git fetch "https://github.com/Professor-Berni/android_system_sepolicy" lineage-23.2 && git cherry-pick 5818a2faf1cdf85cd5802dee354795cd0c795001
+cd $current_dir
